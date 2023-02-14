76ea6d5b50163f0f23c92cd3447bfeffc93ab1ca
abdissa <abdissadegefu1@gmail.com>
2023-02-14 16:58:12 +0300

    Database models initialized

--- src/Model/login.model.ts
+++ src/Model/login.model.ts
@@ -1,13 +1,15 @@
 /* eslint-disable prettier/prettier */
 import * as mongoose from "mongoose";
 export const LoginSchema=new mongoose.Schema({
-    email:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
+    hashedRt:{type:String},
     role:{type:String,required:true}
 })
 export interface Login{
     id:string;
     email:string;
     password:string;
+    hashedRt:string;
     role:string
 }
